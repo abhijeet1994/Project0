c025b07e093a7393403fae92092e4af2ad6b9f9e
Abhijeet Mishra <aam2285@columbia.edu>
2019-12-10 03:20:36 -0500

    just created a packag

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/abhijeet/jobs/robotics/project0/src
+# Build directory: /home/abhijeet/jobs/robotics/project0/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
