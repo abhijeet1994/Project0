5922a8730bfc15f29652a77b33984425c014e0ac
Abhijeet Mishra <aam2285@columbia.edu>
2019-12-11 07:11:54 -0500

    need to send to msgs

--- src/sub1.py
+++ src/sub1.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import rospy
 from std_msgs.msg import String
-
+"""
 def callback(data):
     rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
     
@@ -16,6 +16,36 @@ def listener():
 
     rospy.Subscriber("chatter", String, callback)
 
+    # spin() simply keeps python from exiting until this node is stopped
+    rospy.spin()
+
+if __name__ == '__main__':
+    listener()
+"""
+def callback1(data):
+    a = ( "I heard %s", data.data)
+    print(a)
+    print(a.type)
+    return a
+
+def callback2(data):
+    a =  ( "%s", data.data)
+    print (a)
+    print(a.type)
+    return a
+def listener():
+
+    # In ROS, nodes are uniquely named. If two nodes with the same
+    # name are launched, the previous one is kicked off. The
+    # anonymous=True flag means that rospy will choose a unique
+    # name for our 'listener' node so that multiple listeners can
+    # run simultaneously.
+    rospy.init_node('listener', anonymous=True)
+
+    part2 = rospy.Subscriber("chatter", String, callback1)
+    part1 = rospy.Subscriber("chatter2", String, callback2)
+
+    print (part1 + part2)	
     # spin() simply keeps python from exiting until this node is stopped
     rospy.spin()
 
